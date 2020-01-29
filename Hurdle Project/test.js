fb92b2817df64e5620c939c5ca2cf2d82684f9e2
shwarmaramen101 <igeorge21@nscds.org>
2020-01-29 17:27:50 +0000

    wasd

--- Hurdle Project/test.js
+++ Hurdle Project/test.js
@@ -14,6 +14,22 @@ class Hurdle {
     }
         
 }
+class runner {
+    constructor(x, y, height, width, color) {
+        this.bottom = y;
+        this.left = x;
+        this.height = height;
+        this.width = width;
+        this.color = color;
+        this.addrunner(); // Add the addHurdle function which will append the hurdle to the screen.
+    }
+    addrunner(){
+        console.log(this.bottom,this.left,this.height,this.width,this.color);
+        $('body').append ("<div class='runner' style='bottom:" + this.bottom + "px; left:" + this.left + "px; height: " + this.height + "px; width:" + this.width + "px; background-color:" + this.color + "'></div>");        
+   
+    }
+        
+}
 
 /*getPosition() ;
         return [x, y];*/
@@ -59,8 +75,18 @@ $(document).keydown(function(e) {
     } 
 });             
 
+if (Hurdle1.x < runner.x + Hurdle1.width &&
+   Hurdle1.x + Hurdle1.width > runner.x &&
+   Hurdle1.y < runner.y + runner.height &&
+   Hurdle1.y + Hurdle1.height > runner.y) {
+    // collision detected!
+    alert("You Lose!")
+}
+
+
 
 var Hurdle1 = new Hurdle(200, 225, 65, 17, "darkred");
-var Hurdle1 = new Hurdle(400, 225, 110, 15, "darkred");
-var Hurdle1 = new Hurdle(600, 225, 50, 10, "darkred");
-var Hurdle1 = new Hurdle(800, 225, 125, 15, "darkred");
+var Hurdle2 = new Hurdle(400, 225, 110, 15, "darkred");
+var Hurdle3 = new Hurdle(600, 225, 50, 10, "darkred");
+var Hurdle4 = new Hurdle(800, 225, 125, 15, "darkred");
+var runner = new runner (0, 225, 150, 15, "blue")
