72cee772f5ed8e4a24c63f14816484d421cefdf8
shwarmaramen101 <igeorge21@nscds.org>
2020-02-06 17:42:41 +0000

    asd

--- Hurdle Project/test.js
+++ Hurdle Project/test.js
@@ -21,7 +21,9 @@ class Runner {
         this.height = height;
         this.width = width;
         this.color = color;
-        this.addRunner(); 
+        this.addRunner();
+        this.speed = 50;
+        this.jump = 100;
     }
     addRunner(){
         console.log(this.bottom,this.left,this.height,this.width,this.color);
@@ -31,41 +33,82 @@ class Runner {
         
 }
 
+class Bounce {
+    constructor (translate, scale){
+        this.translate = translate;
+        this.scale = scale;
+
+    }
+
+    }
 
 
 var Hurdle1 = new Hurdle(200, 225, 65, 17, "darkred");
 var Hurdle2 = new Hurdle(400, 225, 110, 15, "darkred");
 var Hurdle3 = new Hurdle(600, 225, 50, 10, "darkred");
 var Hurdle4 = new Hurdle(800, 225, 125, 15, "darkred");
-var runner = new Runner (10, 371, 150, 25, "blue")
+var runner =  newRunner (10, 371, 150, 25, "blue")
+
+var bounce = new Bounce();
+bounce.scale({
+  from: { x: 0.5, y: 0.5 },
+  to: { x: 1, y: 1 }
+})
+bounce.translate({
+  from: { y: 0 },
+  to: { y: 150 }
+});
+
+bounce.define("runner");
+;
 
 
 $(document).keydown(function(e) { 
+ 
 
-    if (e.which == '38') { //up arrow key 
+    if (e.which == '37') { //down arrow key 
+        $(".runner").finish().animate({ 
+            left: "-=50" 
+        }); 
+    } 
+ 
+        if (e.which == '40') { //down arrow key 
         $(".runner").finish().animate({ 
             top: "+=50" 
         }); 
     } 
-
-
-    if (e.which == '40') { //down arrow key 
-        $(".runner").finish().animate({ 
-            top: "-=50" 
-        }); 
-    } 
  
-    if (e.which == '68') { //right arrow key 
+    if (e.which == '39') { //right arrow key 
         $(".runner").finish().animate({ 
-            left: "+=50"
+            left: "+=" + runner.speed  
+            //top: "+=" + runner.jump + 
             
         }); 
-            if (Hurdle1.left < runner.left + Hurdle1.width &&
-            Hurdle1.left + Hurdle1.width > runner.left &&
-            Hurdle1.bottom < runner.bottom + runner.height &&
-            Hurdle1.bottom + Hurdle1.height > runner.bottom) {
-                alert("You Lose!")
-}
-    } 
+        runner.left += runner.speed;
+        //runner.bottom += runner.jump;
+        
+            console.log("runner-height",runner.height);
+            //console.log("runner-width",runner.width);
+            //console.log("runner-right",runner.left+runner.width);
+            //console.log("runner-bottom",runner.bottom);
+            //console.log("hurdle-height",Hurdle1.height);
+            //console.log("hurdle-width",Hurdle1.width);
+            //console.log("hurdle-left",Hurdle1.left);
+            //console.log("hurdle-bottom",Hurdle1.bottom);
+            if (runner.left < Hurdle1.left + Hurdle1.width &&
+            runner.left + runner.width > Hurdle1.left) //&&
+            //runner.bottom < Hurdle1.bottom + Hurdle1.heigh &&
+            //runner.bottom + runner.height > Hurdle1.bottom)
+    
+                alert("You Lose!");
+}             
+
+
+//if (rect1.x < rect2.x + rect2.width &&
+   //rect1.x + rect1.width > rect2.x &&
+   //rect1.y < rect2.y + rect2.height &&
+   //rect1.y + rect1.height > rect2.y) {
+    // collision detected!
+     
 
 });             
