43cbb46f8a5eeedaf7f8d153eda9576f5397885d
shwarmaramen101 <igeorge21@nscds.org>
2020-02-10 15:18:18 +0000

    asd

--- Hurdle Project/test.js
+++ Hurdle Project/test.js
@@ -12,6 +12,7 @@ class Hurdle {
         $('body').append ("<div class='hurdle' style='bottom:" + this.bottom + "px; left:" + this.left + "px; height: " + this.height + "px; width:" + this.width + "px; background-color:" + this.color + "'></div>");        
    
     }
+
         
 }
 class Runner {
@@ -23,33 +24,47 @@ class Runner {
         this.color = color;
         this.addRunner();
         this.speed = 50;
-        this.jump = 100;
+
     }
+    
+
+    }
+
     addRunner(){
         console.log(this.bottom,this.left,this.height,this.width,this.color);
         $('body').append ("<div class='runner' style='bottom:" + this.bottom + "px; left:" + this.left + "px; height: " + this.height + "px; width:" + this.width + "px; background-color:" + this.color + "'></div>");        
    
     }
+/*bounce(){
+        TweenLite.set (".runner",{
+            bottom: 200,
+            left: 0
         
-}
+        });
+        to(".runner", 0.5, {
+        transformOrigin: "50% 50%",
+        y: 90,
+        ease: Bounce.easeOut
+      }, "bounce")
 
-class Bounce {
-    constructor (translate, scale){
-        this.translate = translate;
-        this.scale = scale;
 
-    }
+      .to("runner", 0.4, {
+        transformOrigin: "50% 50%",
+        y: 279,
+        ease: Circ.easeIn
 
-    }
+      }, "bounce2")
+    }*/
+
 
 
 var Hurdle1 = new Hurdle(200, 225, 65, 17, "darkred");
 var Hurdle2 = new Hurdle(400, 225, 110, 15, "darkred");
 var Hurdle3 = new Hurdle(600, 225, 50, 10, "darkred");
 var Hurdle4 = new Hurdle(800, 225, 125, 15, "darkred");
-var runner =  newRunner (10, 371, 150, 25, "blue")
+var runner =  new Runner (10, 371, 150, 25, "blue")
 
-var bounce = new Bounce();
+/*var bounce = new Bounce();
 bounce.scale({
   from: { x: 0.5, y: 0.5 },
   to: { x: 1, y: 1 }
@@ -60,7 +75,7 @@ bounce.translate({
 });
 
 bounce.define("runner");
-;
+; */
 
 
 $(document).keydown(function(e) { 
@@ -85,12 +100,19 @@ $(document).keydown(function(e) {
             
         }); 
         runner.left += runner.speed;
+        bounce()
+        TweenMax.to(".runner", 1.2,{x:0, y:200, ease:Sine.easeOut});
+
+
+
+        }
+
         //runner.bottom += runner.jump;
-        
-            console.log("runner-height",runner.height);
+            
+            //console.log("runner-height",runner.height);
             //console.log("runner-width",runner.width);
             //console.log("runner-right",runner.left+runner.width);
-            //console.log("runner-bottom",runner.bottom);
+            console.log("runner-bottom",runner.bottom);
             //console.log("hurdle-height",Hurdle1.height);
             //console.log("hurdle-width",Hurdle1.width);
             //console.log("hurdle-left",Hurdle1.left);
@@ -101,14 +123,8 @@ $(document).keydown(function(e) {
             //runner.bottom + runner.height > Hurdle1.bottom)
     
                 alert("You Lose!");
+            
 }             
-
-
-//if (rect1.x < rect2.x + rect2.width &&
-   //rect1.x + rect1.width > rect2.x &&
-   //rect1.y < rect2.y + rect2.height &&
-   //rect1.y + rect1.height > rect2.y) {
-    // collision detected!
      
 
 });             
